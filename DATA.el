57a7bd0d0232f3333454d31bee746fd705a869d9
Jason Reed <jcreed@gmail.com>
2017-09-23 21:02:46 -0400

    stuff

---
+++ DATA.el
@@ -0,0 +1,13 @@
+(
+ ("RS"
+  "https://arxiv.org/abs/1705.07442")
+ ("Joyal 97"
+  "https://ncatlab.org/nlab/files/JoyalThetaCategories.pdf"
+  "https://ncatlab.org/nlab/show/Theta+category")
+ ("CCHM"
+  "https://arxiv.org/pdf/1611.02108.pdf")
+ ("ShuFix"
+  "https://arxiv.org/abs/1509.07584")
+ ("Adj2"
+  "http://dlicata.web.wesleyan.edu/pubs/ls15adjoint/ls15adjoint.pdf")
+)
