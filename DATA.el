8789cac92abdc3d329597ffc5bee9a5dd4795d1d
Jason Reed <jcreed@gmail.com>
2017-09-24 08:33:24 -0400

    stuff

--- DATA.el
+++ DATA.el
@@ -1,13 +1,21 @@
 (
  ("RS"
-  "https://arxiv.org/abs/1705.07442")
+  "https://arxiv.org/pdf/1705.07442.pdf"
+  "A type theory for synthetic $\infty$-categories")
  ("Joyal 97"
   "https://ncatlab.org/nlab/files/JoyalThetaCategories.pdf"
+  "Disks, Duality, and $\Theta$-categories"
   "https://ncatlab.org/nlab/show/Theta+category")
  ("CCHM"
-  "https://arxiv.org/pdf/1611.02108.pdf")
+  "https://arxiv.org/pdf/1611.02108.pdf"
+  "Cubical Type Theory: a constructive interpretation of the univalence axiom")
  ("ShuFix"
-  "https://arxiv.org/abs/1509.07584")
+  "https://arxiv.org/pdf/1509.07584.pdf"
+  "Brouwer's fixed-point theorem in real-cohesive homotopy type theory")
  ("Adj2"
-  "http://dlicata.web.wesleyan.edu/pubs/ls15adjoint/ls15adjoint.pdf")
+  "http://dlicata.web.wesleyan.edu/pubs/ls15adjoint/ls15adjoint.pdf"
+  "Adjoint Logic with a 2-Category of Modes")
+ ("Fib2"
+  "http://dlicata.web.wesleyan.edu/pubs/lsr17multi/lsr17multi-ex.pdf"
+  "A Fibrational Framework for Substructural and Modal Logics (extended version)")
 )
