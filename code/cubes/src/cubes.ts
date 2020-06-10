93ae669e4661096b9a40825df570e4a78525f5a0
Jason Reed <jcreed@gmail.com>
2020-06-10 14:23:37 -0400

    working on cubes code a bit

--- code/cubes/src/cubes.ts
+++ code/cubes/src/cubes.ts
@@ -6,7 +6,8 @@ function times<T>(n: number, f: (x: T) => T, x: T): T {
 }
 
 type Dc = 'dom' | 'cod';
-// canonical boundary of *^n
+
+// cube boundary: returns domain or codomain of *^n
 function cbd(n: number, dc: Dc): string[] {
   const rv: string[] = [];
   for (let i = 0; i < n; i++) {
@@ -23,8 +24,9 @@ function cbd(n: number, dc: Dc): string[] {
   return rv;
 }
 
+// cell boundary: returns domain or codomain of s
 function bd(s: string, dc: Dc): string[] {
-  const stars = s.match(/\*/g).length;
+  const stars = (s.match(/\*/g) || []).length;
   const cbds = cbd(stars, dc);
   function expand(tdata: string): string {
     let rv = "";
@@ -43,6 +45,42 @@ function bd(s: string, dc: Dc): string[] {
   return cbds.map(expand);
 }
 
+// element of the ring Z[{0,1,*}^n].
+// represented as mapping strings in {0,1,*}^n to their coefficient.
+// Example: {'*1': 1, '0*': -1}
+type Ring = { [s: string]: number };
+
+// signed boundary, as ring element
+function sbd(s: string): Ring {
+  const rv: Ring = {};
+  bd(s, 'dom').forEach(x => rv[x] = -1);
+  bd(s, 'cod').forEach(x => rv[x] = 1);
+  return rv;
+}
+
+// add m * r into s
+function addTo(m: number, r: Ring, s: Ring): void {
+  for (const k of Object.keys(r)) {
+    if (!s[k]) s[k] = 0;
+    s[k] += m * r[k];
+  }
+}
+
+// return signed boundary of ring element r, as a ring element
+export function sbdr(r: Ring): Ring {
+  const rv: Ring = {};
+  for (const k of Object.keys(r)) {
+    addTo(r[k], sbd(k), rv);
+  }
+  for (const k of Object.keys(rv)) {
+    if (rv[k] == 0)
+      delete rv[k];
+  }
+  return rv;
+}
+
+//console.log(sbdr({ '*0': 1, '0*': -1 }));
+
 type Bound = { cod: string[], dom: string[] };
 
 function fbd(s: string): Bound {
@@ -51,7 +89,7 @@ function fbd(s: string): Bound {
 
 function mbd(b: string[]): Bound {
   const rv: { [k: string]: number } = {};
-  function accum(k, v) {
+  function accum(k: string, v: number) {
     if (!rv[k]) rv[k] = 0;
     rv[k] += v;
   }
@@ -73,11 +111,13 @@ function dmbd(b: Bound): Bound {
   return mbd(b.dom);
 }
 
-const n = 6;
-const k = 3;
-// What I expect this to output is
-// dom and cod both being a representation of n choose k
-// with non-* bits at the 'chosen' positions, with bit values starting at 0
-// with dom, and 1 with cod, and flipping for every *.
-const s = times(n, (a => a + '*'), '');
-console.log(times(k - 1, dmbd, fbd(s)));
+function go() {
+  const n = 6;
+  const k = 2;
+  // What I expect this to output is
+  // dom and cod both being a representation of n choose k
+  // with non-* bits at the 'chosen' positions, with bit values starting at 0
+  // with dom, and 1 with cod, and flipping for every *.
+  const s = times(n, (a => a + '*'), '');
+  console.log(times(k - 1, dmbd, fbd(s)));
+}
