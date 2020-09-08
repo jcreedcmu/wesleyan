cc3ee756f3b675467370039e7d41806355a20f61
Jason Reed <jcreed@gmail.com>
2020-09-08 10:32:05 -0400

    reduce global state

--- python/flag-eigensystems/n-simplex.py
+++ python/flag-eigensystems/n-simplex.py
@@ -9,40 +9,25 @@ import pickle
 import os
 import time
 
-# analyze the flags of the N-simplex
-N = 3
-
-# degree of polynomial / multiplicity of root
-DEG = 6
-
-n = N + 2
-nfac = math.factorial(n)
-
-# takes number in range(nfac), returns permutation
-def decomp(x):
-    return Permutation.from_lehmer(x, n)
-
-# inverse of decomp, takes permutation, returns number
-def comp(x):
-    return x.lehmer(n)
-
-def pi(m):
-  def entry(i):
-    if i == m+1:
-      return m+2
-    elif i == m+2:
-      return m+1
-    else:
-      return i
-  return Permutation(*[entry(i) for i in range(1,n+1)])
-
-def σ(m, x):
-    return comp(decomp(x) * pi(m))
-
-σs = [[σ(m, x) for m in range(n-1)] for x in range(nfac)]
-
-
+# given a list of coefficients for σ0, σ1, ... σn flips,
+# output the corresponding matrix.
 def mkmat(params):
+  n = len(params) + 1
+  nfac = math.factorial(n)
+
+  def pi(m):
+    def entry(i):
+      if i == m+1:
+        return m+2
+      elif i == m+2:
+        return m+1
+      else:
+        return i
+    return Permutation(*[entry(i) for i in range(1,n+1)])
+
+  σs = [[ (Permutation.from_lehmer(x, n) * pi(m)).lehmer(n)
+          for m in range(n-1) ] for x in range(nfac) ]
+
   mat = np.zeros([nfac, nfac])
   for (row, vec) in enumerate(σs):
     for (dim, col) in enumerate(vec):
@@ -65,14 +50,7 @@ def p2s(p, v="x"):
 
 def getEigvals(params, debug=False):
   before = time.perf_counter()
-  cacheFile = f"/tmp/matrix-{n}.pickle"
-  if os.path.exists(cacheFile) and False:
-      with open(cacheFile, 'rb') as cache:
-          mat = pickle.load(cache)
-  else:
-      mat = mkmat(params)
-      with open(cacheFile, 'wb') as cache:
-          pickle.dump(mat, cache)
+  mat = mkmat(params)
   if debug:
     print (f"constructing matrix: {time.perf_counter() - before}")
 
@@ -83,34 +61,45 @@ def getEigvals(params, debug=False):
 
   return [round(x, 11) for x in eigval]
 
-def getPoly(params, multiplicity, debug=False):
+def getPoly(params, mult, indices=None, debug=False):
   count = Counter(getEigvals(params, debug))
 
   accum = np.poly1d([1])
-  for i in reversed([i for i in count.keys()]):
-    if (count[i] == multiplicity):
-      accum *= np.poly1d([1,-i])
+  eigs = [ i for i in count.keys() if count[i] == mult ]
+  if debug:
+    print(eigs)
+  if indices == None:
+    indices = range(len(eigs))
+  for i in indices:
+    accum *= np.poly1d([1,-eigs[i]])
   return accum
 
-polys = []
-for d in range(2,30):
-    poly = getPoly([1,1,2,d], DEG)
-    print(p2s(poly))
-    polys.append(poly)
+DEG=4
 
-for coe in [2,4,6]:
-  xs = range(2, 2+DEG+1)
-  ys = [ round(polys[x-1].coeffs[coe], 3) for x in xs]
-  print (f"x^{6-coe} ( {p2s( lagrange(xs,ys), 'd')} ) +")
+def showPolys(debug=False):
+  polys = []
+  for d in range(2,30):
+      poly = getPoly([1,1,1,d], mult=DEG, indices=[0,1,2,4], debug=debug)
+      print(p2s(poly))
+      polys.append(poly)
 
-exit(0)
-print ("""
+  for coe in [0, 1, 2, 3, 4]:
+    xs = range(2, 2+DEG+1)
+    ys = [ round(polys[x-1].coeffs[coe], 3) for x in xs]
+    print (f"x^{DEG-coe} ( {p2s( lagrange(xs,ys), 'd')} ) +")
+
+def showEigs():
+  count = Counter(getEigvals([1,1,1,1.1]))
+  print ("""
 |--------------+------------|
 | multiplicity | eigenvalue |
 |--------------+------------|""")
-for i in reversed([i for i in count.keys()]):
-    print(f"| {count[i]} | {i} |")
-print("|--------------+------------|")
+  for i in reversed([i for i in count.keys()]):
+      print(f"| {count[i]} | {i} |")
+  print("|--------------+------------|")
 
-keycounts = Counter([count[k] for k in count.keys()])
-print(keycounts)
+  keycounts = Counter([count[k] for k in count.keys()])
+  print(keycounts)
+
+showEigs()
+# showPolys()
