54eb680e65ecf0d2a9a7d441941c6c05260c931c
Jason Reed <jcreed@gmail.com>
2022-07-02 12:59:49 -0400

    post rebalance state generalized

--- code/bch/src/bch-checks.ts
+++ code/bch/src/bch-checks.ts
@@ -3,12 +3,11 @@ import { Exp } from './basics';
 import { spretty, sub } from './lib';
 import { positiveMotion } from './positive-motion';
 import { rebalance } from './rebalance';
-import { postMotion1State, postMotion2State, postRebalance1State, postRebalance2State, postRebalance3State, postSynth2State, postSynth3State, postZeroState } from './state-checkpoints';
+import { postMotion1State, postMotion2State, postRebalanceState, postSynth2State, postSynth3State, postZeroState } from './state-checkpoints';
 import { Phase, Story, synthAll, tellStory } from './synth-and-story';
 import { zeroMotion } from './zero-motion';
 
-const N = 5;
-
+const N = 6;
 
 function makeStep(msg: string, trans: Exp, have: Exp): Phase[] {
   return [[msg, trans], { t: 'check', f: state => assert.equal('0', spretty(sub(state, have))) }];
@@ -37,13 +36,13 @@ const proofN: Story = {
   size: N,
   phases: [
     ...makeStep("move [0,-]", zeroMotion(N), postZeroState(N)),
-    ...makeStep("rebalance ...1", rebalance(N, 1), postRebalance1State(N)),
+    ...makeStep("rebalance ...1", rebalance(N, 1), postRebalanceState(N, 1)),
     ...makeStep("move [-,1]", positiveMotion(N, 1), postMotion1State(N)),
     ...makeStep("synthesize G2", synthAll(N, 2), postSynth2State(N)),
-    ...makeStep("rebalance ...2", rebalance(N, 2), postRebalance2State(N)),
+    ...makeStep("rebalance ...2", rebalance(N, 2), postRebalanceState(N, 2)),
     ...makeStep("move [-,2]", positiveMotion(N, 2), postMotion2State(N)),
     ...makeStep("synthesize G3", synthAll(N, 3), postSynth3State(N)),
-    ...makeStep("rebalance ...3", rebalance(N, 3), postRebalance3State(N)),
+    ...makeStep("rebalance ...3", rebalance(N, 3), postRebalanceState(N, 3)),
     // ["rebalance ...3", rebalance(N, 3)],
     // ["move [-,3]", positiveMotion(N, 3)],
     // ["synthesize G4", synthAll(N, 4)],
