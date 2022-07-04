444c4c46515305d726bf7eff7b80b208682dd5ed
Jason Reed <jcreed@gmail.com>
2022-07-04 13:54:11 -0400

    Validate zero-movement lemma

--- code/bch/src/state-checkpoints.ts
+++ code/bch/src/state-checkpoints.ts
@@ -21,7 +21,15 @@ export function Gp(c: number[]): Exp[] {
 //   return rv;
 // }
 
-function esum(a: number, b: number, f: (i: number) => Exp): Exp {
+export function csum(n: number, f: (c: number[]) => Exp): Exp {
+  let rv = G(0, 0);
+  comps(n).forEach(c => {
+    rv = plus(rv, f(c));
+  });
+  return rv;
+}
+
+export function esum(a: number, b: number, f: (i: number) => Exp): Exp {
   let rv = sep(0, G(0));
   for (let i = a; i <= b; i++) {
     rv = plus(rv, f(i));
@@ -29,34 +37,16 @@ function esum(a: number, b: number, f: (i: number) => Exp): Exp {
   return rv;
 }
 
-// This captures all the expressions that have freshly synthesized 1's
+// This captures all the expressions that have freshly synthesized G_s's
 // in the n→n+1 proof.
-function synth1(n: number) {
-  return plusa(...comps(n).map(c =>
-    sep(factorial(n) / factorial(c.length),
-      proda(...[...c, 1].map(x => G(x))))
-  ));
-}
-
-// This captures all the expressions that have freshly synthesized 2's
-// in the n→n+1 proof.
-function synth2(n: number) {
-  return plusa(...comps(n - 1).map(c =>
-    sep(2 * factorial(n) / factorial(c.length),
-      proda(...[...c, 2].map(x => G(x))))
-  ));
-}
-
-// This captures all the expressions that have freshly synthesized s's
-// in the n→n+1 proof.
-function synths(n: number, s: number) {
+export function synths(n: number, s: number) {
   return plusa(...comps(n + 1 - s).map(c =>
     sep(s * factorial(n) / factorial(c.length),
       proda(...[...c, s].map(x => G(x))))
   ));
 }
 
-function cartprod<T, U>(ts: T[], us: U[]): [T, U][] {
+export function cartprod<T, U>(ts: T[], us: U[]): [T, U][] {
   let rv: [T, U][] = [];
   ts.forEach(t => {
     us.forEach(u => {
@@ -67,7 +57,7 @@ function cartprod<T, U>(ts: T[], us: U[]): [T, U][] {
 }
 
 // returns G_{1⋯1} with n 1's
-function fullySplit(n: number) {
+export function fullySplit(n: number) {
   let rv = G(1);
   for (let i = 0; i < n - 1; i++) {
     rv = prod(rv, G(1));
@@ -85,7 +75,7 @@ assert.equal('G_{11111}', spretty(fullySplit(5)));
 // which are going to be used for m-synthesis. For example:
 // - G_{23[022]} has n=(2+3)+(2+2)=9, and m=(2+2)+1=5.
 // - G_{4[01]} has n=(4)+(1)=5, and m=(1)+1=2.
-function zeroSwaps(n: number, m: number) {
+export function zeroSwaps(n: number, m: number) {
   return plusa(
     ...cartprod(comps(n - m + 1), comps(m - 1)).map(([lam1, lam2]) => {
       const coeff =
@@ -105,7 +95,7 @@ assert.ok(spretty(zeroSwaps(5, 2)).includes('120G_{4[0,1]}'));
 // λ₁ s λ₂
 // so n₁ = Σλ₁ is going to range from 0 to n+1-s
 // and n₂ = n + 1 - s - n₁
-function balanceds(n: number, s: number): Exp {
+export function balanceds(n: number, s: number): Exp {
   return esum(0, n + 1 - s, n1 => {
     const n2 = n + 1 - s - n1;
     return plusa(
@@ -127,7 +117,7 @@ function balanceds(n: number, s: number): Exp {
 // λ₁[b,s]λ₂
 // where b ranges from 1 to (n+1)-s, but not equalling s, because
 // we have a total of (n+1)-s-b to spread around λ₁ and λ₂
-function swapss(n: number, s: number): Exp {
+export function swapss(n: number, s: number): Exp {
   return esum(1, (n + 1) - s, b => {
     if (b == s)
       return G(0, 0);
@@ -151,7 +141,7 @@ function swapss(n: number, s: number): Exp {
 
 // Returns all the right-aligned swaps that arise from rebalancing s, which
 // are used for m-synthesis
-function rightSwapss(n: number, m: number, s: number): Exp {
+export function rightSwapss(n: number, m: number, s: number): Exp {
   // the structure of the swap is going to be
   // λ₁ [b, s, λ₂]
   // where Σλ₁ = n₁, Σλ₂ = n₂
@@ -188,7 +178,7 @@ function rightSwapss(n: number, m: number, s: number): Exp {
 
 export function postZeroState(n: number): Exp {
   return plusa(
-    synth1(n),
+    synths(n, 1),
     esum(2, n + 1, m => zeroSwaps(n, m)),
   );
 }
